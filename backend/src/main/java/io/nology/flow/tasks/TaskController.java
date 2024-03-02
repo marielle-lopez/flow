a3fef313420ad863c89df1feb960c093257a9356
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-02 22:20:59 +1100

    removed @CrossOrigin annotation from controller

--- backend/src/main/java/io/nology/flow/tasks/TaskController.java
+++ backend/src/main/java/io/nology/flow/tasks/TaskController.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 import io.nology.flow.exceptions.NotFoundException;
 import jakarta.validation.Valid;
 
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
