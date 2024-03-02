cbf0079e3ab4f9dc7b99b30a84686159cb3f4457
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-02 13:12:55 +1100

    configured content path and added custom colours

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      shark: '#181A20',
+      charade: '#262A34',
+      mercury: '#E7E6E6',
+      'river-bed': '#434A5B',
+      hopbush: '#D680B9',
+      chetwode: '#84A2D8',
+      aquamarine: '#69FFC2',
+    },
   },
   plugins: [],
-}
-
+};
