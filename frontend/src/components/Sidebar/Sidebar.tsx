598d8699f042d04fc30ffac39b4548a5e359b6d1
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-03 21:37:56 +1100

    added links to routes and added corresponding icons to links

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,17 +1,20 @@
-const sidebarWrapperStyles = `
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+  const sidebarWrapperStyles = `
   flex 
   flex-col 
-  basis-full 
+  w-1/5 
   bg-charade
 `;
 
-const logoStyles = `
+  const logoStyles = `
   font-accent
   text-5xl
   font-extralight
 `;
 
-const logoWStyles = `
+  const logoWStyles = `
   font-normal
   text-transparent
   bg-clip-text
@@ -21,12 +24,69 @@ const logoWStyles = `
   to-aquamarine
 `;
 
-const Sidebar = () => {
+  const iconStyles = `
+    inline 
+    w-7 
+  `;
+
   return (
     <div className={sidebarWrapperStyles}>
       <h1 className={logoStyles}>
         flo<span className={logoWStyles}>w</span>
       </h1>
+
+      <ul>
+        <li>
+          <Link to="/">
+            <img
+              className={iconStyles}
+              src="../src/assets/icons/all.png"
+              alt="All icon"
+            />
+            All
+          </Link>
+        </li>
+        <li>
+          <Link to="/today">
+            <img
+              className={iconStyles}
+              src="../src/assets/icons/today.png"
+              alt="Today icon"
+            />
+            Today
+          </Link>
+        </li>
+        <li>
+          <Link to="/inbox">
+            <img
+              className={iconStyles}
+              src="../src/assets/icons/inbox.png"
+              alt="Inbox icon"
+            />
+            Inbox
+          </Link>
+        </li>
+        <li>
+          <Link to="/personal">
+            <img
+              className={iconStyles}
+              src="../src/assets/icons/personal.png"
+              alt="Personal icon"
+            />
+            Personal
+          </Link>
+        </li>
+        <li>
+          <Link to="/work">
+            <img
+              className={iconStyles}
+              src="../src/assets/icons/work.png"
+              alt="Work icon"
+            />
+            Work
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
