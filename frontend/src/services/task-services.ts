65cd63105d1d74b4a5011236f08c1dcf3bb6c029
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-03 21:36:02 +1100

    removed unused code

--- frontend/src/services/task-services.ts
+++ frontend/src/services/task-services.ts
@@ -1,36 +1,36 @@
 export const getAllTasks = async () => {
-    const response = await fetch('http://localhost:8080/tasks');
-    const tasks = await response.json();
-    return tasks;
-}
+  const response = await fetch('http://localhost:8080/tasks');
+  const tasks = await response.json();
+  return tasks;
+};
 
 export const getTaskById = async (id: number) => {
-    const response = await fetch(`http://localhost:8080/tasks/${id}`);
-    const task = await response.json();
-    return task;
-}
+  const response = await fetch(`http://localhost:8080/tasks/${id}`);
+  const task = await response.json();
+  return task;
+};
 
 export const createTask = async () => {
-    const task = {
-        title: 'Frontend task',
-        description: 'Hello from the frontend!',
-        dueAt: '1999-12-22',
-        category: 'Test'
-    };
+  const task = {
+    title: 'Frontend task',
+    description: 'Hello from the frontend!',
+    dueAt: '1999-12-22',
+    category: 'Test',
+  };
 
-    const response = await fetch('http://localhost:8080/tasks', {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        method: 'POST',
-        body: JSON.stringify(task),
-    });
-    const data = await response.json();
-    return data;
-}
+  const response = await fetch('http://localhost:8080/tasks', {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify(task),
+  });
+  const data = await response.json();
+  return data;
+};
 
 export const deleteTaskById = async (id: number) => {
-    const response = await fetch(`http://localhost:8080/tasks/${id}`, {
-        method: 'DELETE',
-    });
-}
+  await fetch(`http://localhost:8080/tasks/${id}`, {
+    method: 'DELETE',
+  });
+};
