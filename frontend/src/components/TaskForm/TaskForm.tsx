bf23013035d2b2482210374c31bdac4ecd25f757
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-03 16:52:27 +1100

    used react hook form to handle form for creating a task

--- frontend/src/components/TaskForm/TaskForm.tsx
+++ frontend/src/components/TaskForm/TaskForm.tsx
@@ -1,37 +1,56 @@
+import { useForm } from 'react-hook-form';
+
 const inputStyles = `
   rounded-md 
   border-gray-200 
   text-gray-800
 `;
 
-const TaskForm = () => {
+const TaskForm = ({ taskFormSubmit = (data: object) => {} }) => {
+  const { register, handleSubmit } = useForm();
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(taskFormSubmit)}>
       <div>
         <label htmlFor="title">Title</label>
-        <input className={inputStyles} type="text" id="title" />
+        <input
+          className={inputStyles}
+          type="text"
+          id="title"
+          {...register('title')}
+        />
       </div>
 
       <div>
         <label htmlFor="desc">Description</label>
-        <input className={inputStyles} type="text" id="desc" />
+        <input
+          className={inputStyles}
+          type="text"
+          id="desc"
+          {...register('description')}
+        />
       </div>
 
       <div>
         <label htmlFor="dueDate">Due Date</label>
-        <input className={inputStyles} type="date" id="dueDate" />
+        <input
+          className={inputStyles}
+          type="date"
+          id="dueDate"
+          {...register('dueDate')}
+        />
       </div>
 
       <div>
         <label htmlFor="category">Category</label>
-        <select className={inputStyles} id="category">
+        <select className={inputStyles} id="category" {...register('category')}>
           <option value="personal">Personal</option>
           <option value="work">Work</option>
           <option value="university">University</option>
         </select>
       </div>
 
-      <button type="submit">Add task</button>
+      <button>Add task</button>
     </form>
   );
 };
