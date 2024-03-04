282b5c6178049e0bbb759147cf97120c40f99268
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-05 10:54:43 +1100

    added logic for retrieving all tasks and implemented TaskList component

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from 'react';
 import MainWrapper from '../../containers/MainWrapper/MainWrapper';
+import TaskList from '../../containers/TaskList/TaskList';
+import { getAllTasks } from '../../services/task-services';
 
 const HomePage = () => {
-  return <MainWrapper>HomePage</MainWrapper>;
+  const [allTasks, setAllTasks] = useState([]);
+
+  useEffect(() => {
+    getAllTasks()
+      .then((res) => setAllTasks(res))
+      .catch((e) => console.warn(e.message));
+  }, []);
+
+  return (
+    <MainWrapper>
+      <TaskList tasks={allTasks} />
+    </MainWrapper>
+  );
 };
 
 export default HomePage;
