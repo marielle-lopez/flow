fb97d75c9a907933146d7d677f1de341a26d2b19
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-07 15:44:39 +1100

    added modalTask state to keep track of the task the modal should show, and renamed states to avoid confusion in other files

--- frontend/src/context/ModalContextProvider.tsx
+++ frontend/src/context/ModalContextProvider.tsx
@@ -3,10 +3,16 @@ import { createContext, useState } from 'react';
 export const ModalContext = createContext(null);
 
 const ModalContextProvider = ({ children }) => {
-  const [isHidden, setIsHidden] = useState(true);
+  const [modalIsHidden, setModalIsHidden] = useState(true);
+  const [modalTask, setModalTask] = useState(null);
+
+  console.log(modalIsHidden);
+  console.log(modalTask);
 
   return (
-    <ModalContext.Provider value={{ isHidden, setIsHidden }}>
+    <ModalContext.Provider
+      value={{ modalIsHidden, setModalIsHidden, modalTask, setModalTask }}
+    >
       {children}
     </ModalContext.Provider>
   );
