36cd1a36ed26177c535cb3d4ceb097ea9026ae7d
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-08 15:02:47 +1100

    created a Category entity

---
+++ backend/src/main/java/io/nology/flow/categories/Category.java
@@ -0,0 +1,38 @@
+package io.nology.flow.categories;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import io.nology.flow.common.BaseEntity;
+import io.nology.flow.tasks.Task;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+public class Category extends BaseEntity {	
+	@Column(unique = true)
+	private String name;
+	
+	@OneToMany(mappedBy = "category")
+	@JsonIgnoreProperties("category")
+	private List<Task> tasks;
+	
+	public List<Task> getTasks() {
+		return tasks;
+	}
+	
+	public Category() {
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
