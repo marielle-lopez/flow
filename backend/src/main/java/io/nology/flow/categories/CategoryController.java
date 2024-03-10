18835a31b446d3f56c07aaa3d1d0fdefe7491f55
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-10 20:27:01 +1100

    added annotations to modify displayed names of controllers and add summaries to each endpoint to describe their function in OpenAPI docs

--- backend/src/main/java/io/nology/flow/categories/CategoryController.java
+++ backend/src/main/java/io/nology/flow/categories/CategoryController.java
@@ -11,15 +11,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/categories")
+@Tag(name = "Categories")
 public class CategoryController {
 	@Autowired
 	private CategoryService categoryService;
 
 	@PostMapping
+	@Operation(summary = "Creates a category")
 	public ResponseEntity<Category> create(@Valid @RequestBody CreateCategoryDTO data) {
 		Category createdCategory = this.categoryService.create(data);
 		return new ResponseEntity<>(createdCategory, HttpStatus.CREATED);
@@ -27,6 +31,7 @@ public class CategoryController {
 	}
 
 	@GetMapping
+	@Operation(summary = "Gets all categories")
 	public ResponseEntity<List<Category>> findAll() {
 		List<Category> allCategories = this.categoryService.findAll();
 		return new ResponseEntity<>(allCategories, HttpStatus.OK);
