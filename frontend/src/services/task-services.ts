bedb344181b918fbe227db242161f40566452e1f
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-12 22:17:29 +1100

    added checks for correct response status code for error handling

--- frontend/src/services/task-services.ts
+++ frontend/src/services/task-services.ts
@@ -6,18 +6,33 @@ export const createTask = async (task: Task) => {
     method: 'POST',
     body: JSON.stringify(task),
   });
+
+  if (response.status !== 201) {
+    throw new Error('Failed to create task');
+  }
+
   const data = await response.json();
   return data;
 };
 
 export const getAllTasks = async () => {
   const response = await fetch('http://localhost:8080/tasks');
+
+  if (!response.ok) {
+    throw new Error('Failed to get tasks');
+  }
+
   const tasks = await response.json();
   return tasks;
 };
 
 export const getTaskById = async (id: number) => {
   const response = await fetch(`http://localhost:8080/tasks/${id}`);
+
+  if (!response.ok) {
+    throw new Error(`Failed to get task ${id}`);
+  }
+
   const task = await response.json();
   return task;
 };
@@ -31,11 +46,16 @@ export const updateTaskById = async (id: number, data: object) => {
     body: JSON.stringify(data),
   });
 
+  if (!response.ok) {
+    throw new Error(`Failed to update task ${id}`);
+  }
+
   const updatedTask = await response.json();
   return updatedTask;
 };
 
 export const deleteTaskById = async (id: number) => {
+  // add error handling?
   await fetch(`http://localhost:8080/tasks/${id}`, {
     method: 'DELETE',
   });
