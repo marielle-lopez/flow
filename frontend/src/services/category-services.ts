845c3e2ecbf848408cc3894b9fcf0adb218de60b
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-13 23:08:09 +1100

    added error handling for category-related API calling

--- frontend/src/services/category-services.ts
+++ frontend/src/services/category-services.ts
@@ -2,5 +2,26 @@ export const getAllCategories = async () => {
   const response = await fetch('http://localhost:8080/categories');
   const data = await response.json();
 
+  if (!response.ok) {
+    throw new Error('Failed to get all categories');
+  }
+
+  return data;
+};
+
+export const createCategory = async (category: Category) => {
+  const response = await fetch('http://localhost:8080/categories', {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify(category),
+  });
+
+  if (response.status !== 201) {
+    throw new Error('Failed to create category');
+  }
+
+  const data = await response.json();
   return data;
 };
