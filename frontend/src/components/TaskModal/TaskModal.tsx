bf0544109e69cf24d08865879d30eeb45f8628a7
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-13 12:43:28 +1100

    re-introduced category as a updatable task property

--- frontend/src/components/TaskModal/TaskModal.tsx
+++ frontend/src/components/TaskModal/TaskModal.tsx
@@ -26,7 +26,7 @@ const TaskModal = () => {
     dueAt: z.coerce.date().min(new Date(Date.now() - 864e5), {
       message: 'Due date cannot be past',
     }),
-    // categoryId: z.coerce.number().int().gte(1),
+    categoryId: z.coerce.number().int().gte(1),
   });
 
   const {
@@ -39,7 +39,7 @@ const TaskModal = () => {
     defaultValues: {
       title: modalTask?.title,
       dueAt: modalTask?.dueAt.split('T', 1)[0],
-      // categoryId: modalTask?.categoryId,
+      categoryId: modalTask?.categoryId,
     },
   });
 
@@ -122,7 +122,7 @@ const TaskModal = () => {
     reset({
       title: modalTask?.title,
       dueAt: modalTask?.dueAt.split('T', 1)[0],
-      // categoryId: modalTask?.categoryId,
+      categoryId: modalTask?.categoryId,
     });
   }, [modalTask]);
 
@@ -176,7 +176,7 @@ const TaskModal = () => {
                   {...register('dueAt')}
                 />
                 {errors.dueAt?.message && <p>{errors.dueAt.message}</p>}
-                {/* <select
+                <select
                   className={inputStyles + ` rounded-tr-full rounded-br-full`}
                   id="categoryId"
                   {...register('categoryId')}
@@ -189,7 +189,7 @@ const TaskModal = () => {
                 </select>
                 {errors.categoryId?.message && (
                   <p>{errors.categoryId.message}</p>
-                )} */}
+                )}
               </div>
               <div className={footerStyles}>
                 <button className="px-4 py-2 rounded-lg text-shark bg-chetwode">
